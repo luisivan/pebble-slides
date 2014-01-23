c4d5a3381716d744b6309e7d62bef860b985a4a5
Luis Iván <me@luisivan.net>
2014-01-23 21:28:06 +0100

    Introducing notes support

--- src/js/pebble-js-app.js
+++ src/js/pebble-js-app.js
@@ -1,14 +1,82 @@
+var CHUNKS_LENGTH = 512
+
 var Utils = {
 	http: function(ip, button, cb, errcb) {
 		var req = new XMLHttpRequest()
 	  	req.open('GET', 'http://'+ip+':8686/'+button, true)
 	  	req.send()
+	},
+	sendQueue: function (queue) {
+        var index = retries = 0
+
+        var doo = function() {
+            if (!queue[index]) return
+
+            console.log('sending '+JSON.stringify(queue[index]))
+            Pebble.sendAppMessage(queue[index], success, fail)
+        }
+        var success = function() {
+            console.log('Packet sent')
+            index += 1
+            retries = 0
+            doo()
+        }
+        var fail = function () {
+            retries += 1
+            if (retries == 3){
+                console.log('Packet fails, moving on')
+                index += 1
+            }
+            doo()
+        }
+        doo()
+	},
+	send: function(data) {
+		console.log(data)
+		var chunks = Math.ceil(data.length/CHUNKS_LENGTH),
+			queue = []
+
+		console.log(chunks)
+                
+        for (var i = 0; i < chunks; i++){
+            var payload = {note:data.substring(CHUNKS_LENGTH*i, CHUNKS_LENGTH*(i+1))}
+            if (i == 0) payload.start = "yes"
+            if (i == chunks-1) payload.end = "yes"
+
+            queue.push(payload)
+        }
+
+        Utils.sendQueue(queue)
 	}
 }
 
-var IP = localStorage.getItem('ip') || '192.168.1.129'
+var IP = localStorage.getItem('ip') || '192.168.0.2',
+	notes = JSON.parse(localStorage.getItem('notes')),
+	currentSlide = 0
+
 
 Pebble.addEventListener("appmessage", function(e) {
 	var button = Object.keys(e.payload)[0]
 	Utils.http(IP, button)
+	if (button == 'up')
+		currentSlide--
+	else if (button == 'down')
+		currentSlide++
+
+	console.log(notes[currentSlide])
+	setTimeout(function() {
+		Utils.send(notes[currentSlide])
+	}, 500)
+})
+
+Pebble.addEventListener("showConfiguration", function (e) {
+	Pebble.openURL("http://luisivan.net/pebble-slides/")
+})
+
+Pebble.addEventListener("webviewclosed", function (e) {
+	if (!e.response) return
+	var payload = JSON.parse(e.response)
+	localStorage.setItem('ip', payload.ip)
+	localStorage.setItem('notes', JSON.stringify(payload.notes))
+	notes = payload.notes
 })
--- src/pebble-slides.c
+++ src/pebble-slides.c
@@ -2,6 +2,8 @@
 
 static Window *window;
 static TextLayer *time_layer;
+static ScrollLayer *scroll_layer;
+static TextLayer *text_layer;
 
 static AppTimer* timer;
 static double min = 0;
@@ -9,9 +11,13 @@ static double min = 0;
 enum {
   KEY_UP = 0x0,
   KEY_SELECT = 0x1,
-  KEY_DOWN = 0x2
+  KEY_DOWN = 0x2,
+  KEY_START = 0x3,
+  KEY_END = 0x4,
+  KEY_NOTE = 0x5
 };
 
+// Send keys
 void send(uint8_t k) {
   DictionaryIterator *it;
   app_message_outbox_begin(&it);
@@ -21,6 +27,70 @@ void send(uint8_t k) {
   app_message_outbox_send();
 }
 
+
+// Text
+
+char note[1024];
+#define spacing 10
+
+static void view_load(Window *window) {
+  Layer *window_layer = window_get_root_layer(window);
+  GRect bounds = layer_get_bounds(window_layer);
+  GRect max_text_bounds = GRect(0, 0, bounds.size.w, 2000);
+
+  scroll_layer = scroll_layer_create(bounds);
+  scroll_layer_set_click_config_onto_window(scroll_layer, window);
+
+  text_layer = text_layer_create(GRect(0, 0, max_text_bounds.size.w, max_text_bounds.size.h));
+  text_layer_set_text(text_layer, note);
+  text_layer_set_font(text_layer, fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD));
+
+  GSize max_size = text_layer_get_content_size(text_layer);
+  text_layer_set_size(text_layer, GSize(max_size.w, max_size.h+spacing));
+
+  scroll_layer_set_content_size(scroll_layer, GSize(bounds.size.w, max_size.h));
+  scroll_layer_add_child(scroll_layer, text_layer_get_layer(text_layer));
+
+  scroll_layer_set_click_config_onto_window(scroll_layer, window);
+
+  layer_add_child(window_layer, scroll_layer_get_layer(scroll_layer));
+}
+
+static void view_unload(Window *window) {
+  text_layer_destroy(text_layer);
+  scroll_layer_destroy(scroll_layer);
+  window_destroy(window);
+}
+
+void view() {
+  Window *view = window_create();
+  window_set_window_handlers(view, (WindowHandlers) {
+      .load = view_load,
+      .unload = view_unload,
+  });
+  window_stack_push(view, true);
+}
+
+// Events
+static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
+  view();
+}
+
+static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send(KEY_UP);
+}
+
+static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
+  send(KEY_DOWN);
+}
+
+static void click_config_provider(void *context) {
+  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
+  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
+  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
+}
+
+// Stopwatch
 void update_stopwatch() {
   static char big_time[] = "00:00";
 
@@ -39,25 +109,17 @@ void handle_timer(void* data) {
   update_stopwatch();
 }
 
+// Notes
 
-static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
-  send(KEY_SELECT);
+static void msg_received(DictionaryIterator *iter, void *context) {
+  if (dict_find(iter, KEY_START)) strcpy(note, "");
+
+  strcat(note, dict_find(iter, KEY_NOTE)->value->cstring);
+
+  if (dict_find(iter, KEY_END)) view();
 }
 
-static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
-  send(KEY_UP);
-}
-
-static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
-  send(KEY_DOWN);
-}
-
-static void click_config_provider(void *context) {
-  window_single_click_subscribe(BUTTON_ID_SELECT, select_click_handler);
-  window_single_click_subscribe(BUTTON_ID_UP, up_click_handler);
-  window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
-}
-
+// Main window
 static void window_load(Window *window) {
   Layer *window_layer = window_get_root_layer(window);
   GRect bounds = layer_get_bounds(window_layer);
@@ -74,7 +136,8 @@ static void window_unload(Window *window) {
 }
 
 static void init(void) {
-  app_message_open(1, 20);
+  app_message_register_inbox_received(msg_received);
+  app_message_open(app_message_inbox_size_maximum(), 20);
 
   timer = app_timer_register(60000, handle_timer, NULL);
 
