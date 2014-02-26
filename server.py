9c02b9f61f7ed392a124c63598b13cd966956283
Tyler H <tyhoffman1@gmail.com>
2014-02-25 23:12:16 -0500

    Works with Mavericks

--- server.py
+++ server.py
@@ -1,23 +1,32 @@
-#!/usr/bin/env python2
-from pykeyboard import PyKeyboard
+#!/usr/bin/env python
+import os
+import sys
 import socket
 from wsgiref.simple_server import make_server
 from ws4py.websocket import WebSocket
 from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
 
-k = PyKeyboard()
-
 class PebbleWebSocket(WebSocket):
+
 	def received_message(self, message):
 
 		if message.data == 'down':
-			k.tap_key(k.right_key)
+			cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 29)'"
+			os.system(cmd)
 		elif message.data == 'up':
-			k.tap_key(k.left_key)
+			cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 28)'"
+			os.system(cmd)
 
+port = 12345
 
-server = make_server('', 0, server_class=WSGIServer,
+if ( 1 < len(sys.argv) < 3 and int(sys.argv[1] >= 1024) ):
+	port = int(sys.argv[1])
+else:
+	print("usage: %s <port (1024+)>" % sys.argv[0])
+	sys.exit()
+
+server = make_server('', port, server_class=WSGIServer,
 					 handler_class=WebSocketWSGIRequestHandler,
 					 app=WebSocketWSGIApplication(handler_cls=PebbleWebSocket))
 
