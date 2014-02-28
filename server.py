c655b52383968df4e01474adf5e707e7ad38bf07
Tyler H <tyhoffman1@gmail.com>
2014-02-28 14:00:00 -0500

    add Mac support

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python
-import os
+
 import sys
 import socket
 from wsgiref.simple_server import make_server
@@ -7,23 +7,40 @@ from ws4py.websocket import WebSocket
 from ws4py.server.wsgirefserver import WSGIServer, WebSocketWSGIRequestHandler
 from ws4py.server.wsgiutils import WebSocketWSGIApplication
 
+# return true if Mac
+def isMac():
+	return sys.platform == 'darwin'
+
+# PyKeyboard does not work on Mac
+if isMac():
+	import os
+else:
+	from pykeyboard import PyKeyboard
+
 class PebbleWebSocket(WebSocket):
 
 	def received_message(self, message):
 
 		if message.data == 'down':
-			cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 29)'"
-			os.system(cmd)
+			if isMac():
+				cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 29)'"
+				os.system(cmd)
+			else:
+				PyKeyboard().tap_key(k.right_key)
+		
 		elif message.data == 'up':
-			cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 28)'"
-			os.system(cmd)
+			if isMac():
+				cmd = "osascript -e 'tell application \"System Events\" to keystroke (ASCII character 28)'"
+				os.system(cmd)
+			else:
+				PyKeyboard().tap_key(k.left_key)
 
-port = 12345
-
-if ( 1 < len(sys.argv) < 3 and int(sys.argv[1] >= 1024) ):
+# either take a port from arguments or serve on random port
+port = 0
+if (len(sys.argv) == 2) and int(sys.argv[1] >= 1024) :
 	port = int(sys.argv[1])
-else:
-	print("usage: %s <port (1024+)>" % sys.argv[0])
+elif len(sys.argv) != 1:
+	print("usage: %s [port]" % sys.argv[0])
 	sys.exit()
 
 server = make_server('', port, server_class=WSGIServer,
